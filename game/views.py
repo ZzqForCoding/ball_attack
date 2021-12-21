4b2ccc5743ce88d94a12ceaa3a2a25c24948c416
子清 周 <17687952609@163.com>
2021-12-21 10:55:56 +0800

    write my first pages

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://img1.baidu.com/it/u=2117192636,2944328342&fm=26&fmt=auto" width=800>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line2 = '<a href="/">返回主页面</a>'
+    return HttpResponse(line1 + line2)
