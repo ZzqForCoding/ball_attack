d43f77f545d8728165fe456216162f2b013cc2dc
子清 周 <17687952609@163.com>
2021-12-27 17:00:31 +0800

    write naive system and register

--- acapp/settings.py
+++ acapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=)ad2j$=3l=mm1jsi6z-70b@pay0xsc^zdi4g8kk_q8gxjj-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["120.76.157.21", "app975.acapp.acwing.com.cn"]
 
