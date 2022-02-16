3fc66339547cbc83a3e2ca6d379dde6b8d5f3191
子清 周 <17687952609@163.com>
2022-02-16 12:31:59 +0800

    update player of minimap and horizon of minimap

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -423,10 +423,11 @@ class MiniMap extends AcGameObject {
         this.$canvas = $(`<canvas class="ac-game-mini_map"></canvas>`);
         this.ctx = this.$canvas[0].getContext('2d');
         this.background_color = "rgba(0, 0, 0, 0.3)";
-        this.player_color = "rgba(255, 0, 0, 0.3)";     // 敌方颜色
 
         this.players = this.playground.players; // 死了还会画出来吗(赋的是引用还是值)
         this.virtual_map_width = this.virtual_map_height = this.playground.virtual_map_width;
+
+        this.mode = this.playground.mode;
 
         this.playground.$playground.append(this.$canvas);
     }
@@ -441,6 +442,9 @@ class MiniMap extends AcGameObject {
 
         this.width = this.ctx.canvas.width;
         this.height = this.ctx.canvas.height;
+        // 大地图转化为小地图的换算关系
+        this.rate_width = this.width / this.virtual_map_width;
+        this.rate_height = this.height / this.virtual_map_height;
 
         // $playground的宽高是包括灰色区域以及游戏界面 - canvas宽高 = 左右的灰色区域 / 2 = 右边的灰色区域 + 距离canvas右下角的距离
         this.margin_right = (this.playground.$playground.width() - this.playground.width) / 2 + this.playground.height * 0.02;
@@ -462,24 +466,39 @@ class MiniMap extends AcGameObject {
         this.ctx.clearRect(0, 0, this.width, this.height);  // 清除绘画图形默认涂的黑色
         this.ctx.fillStyle = this.background_color;
         this.ctx.fillRect(0, 0, this.width, this.height);
+
         for(let i = 0; i < this.players.length; i++) {
-            let x = (this.players[i].x - this.playground.cx) / this.virtual_map_width * this.width;
-            let y = (this.players[i].y - this.playground.cy) / this.virtual_map_height * this.height;
-            if(i == 0) {
+            let x = this.players[i].x * this.rate_width;
+            let y = this.players[i].y * this.rate_height;
+            let r = this.players[i].radius * this.width;     // 玩家小地图上的半径
+            if(i === 0 || this.mode === "multi mode") {
                 this.ctx.save();
                 this.ctx.beginPath();
-                this.ctx.arc(x, y, 0.05 * this.width, 0, Math.PI * 2, false);
+                this.ctx.arc(x, y, r, 0, Math.PI * 2, false);
                 this.ctx.stroke();
                 this.ctx.clip();
-                this.ctx.drawImage(this.players[i].img, (x - (0.05 * this.width)), 0.05 * this.width, 0.05 * this.width * 2, 0.05 * this.width * 2);
+                this.ctx.drawImage(this.players[i].img, (x - r), (y - r), r * 2, r * 2);
                 this.ctx.restore();
             } else {
                 this.ctx.beginPath();
-                this.ctx.arc(x, y, 0.05 * this.width, 0, Math.PI * 2, false);
-                this.ctx.fillStyle = this.player_color;
+                this.ctx.arc(x, y, r, 0, Math.PI * 2, false);
+                this.ctx.fillStyle = this.players[i].color;
                 this.ctx.fill();
             }
         }
+
+        // 视野矩形左上角的点
+        let tx = (this.players[0].x - this.playground.width / 2 / scale) * this.rate_width;
+        let ty = (this.players[0].y - this.playground.height / 2 / scale) * this.rate_height;
+        // 视野矩形的宽和高
+        let w = this.playground.width / scale * this.rate_width;
+        let h = this.playground.height / scale * this.rate_height;
+        this.ctx.save();
+        this.strokeStyle = "rgba(247, 232, 200, 0.7)";
+        this.ctx.setLineDash([15, 5]);
+        this.ctx.lineWidth = Math.ceil(3 * scale / 1080);
+        this.ctx.strokeRect(tx, ty, w, h);
+        this.ctx.restore();
     }
 }
 class NoticeBoard extends AcGameObject {
